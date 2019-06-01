248311b48943d02c89bfe001648b465f8243ca15
HemabhKamboj <hkamboj2603199@gmail.com>
2019-06-01 17:35:51 +0530

    added car data api

---
+++ source/application.py
@@ -0,0 +1,31 @@
+from flask import Flask, session, request, render_template, redirect, jsonify, flash
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+
+
+engine = create_engine("postgres://dwtwkhpihgaedb:866d89ea4688710f804d0258d799588a39aa40b738a3e9cbe99795b33e2e0241@ec2-54-225-72-238.compute-1.amazonaws.com:5432/deik5m0otb9kgc")
+
+@app.route("/cardata", methods=["GET"])
+def cardata():
+    """ Get car data """
+
+    # Check car id was provided
+    if not request.args.get("carid"):
+        return "you must provide a car id."
+
+    # # Take input and add a wildcard
+    # query =request.args.get("carid")
+
+    # # Capitalize all words of input for search
+    # # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
+    # query = query.title()
+
+    rows = engine.execute("SELECT * FROM car_data WHERE car_id =1")
+                    #   {"query":request.args.get("carid")})
+
+
+    return jsonify({'rows': [dict(row) for row in rows]})
+    # result = dict(rows)
+
+    # return jsonify(result)                  
